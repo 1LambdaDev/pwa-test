bb049aeed5b46bbe8b42d1104b73aa95637ccb22
1LambdaDev <vlad.chikivdia@gmail.com>
2023-04-09 18:04:24 +0300

    Add pwa app

---
+++ src/sw.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", e=> {
+    console.log("Install!");
+    e.waitUntil(
+        caches.open('static').then(cache => {
+            return cache.addAll([
+                './',
+                './css/styles.css',
+                './logo_images/logo150.png',
+                './js/index.js',
+                './js/app.js'
+            ])
+        })
+    )
+});
+
+self.addEventListener("fetch", e=> {
+    e.respondWith(
+        caches.match(e.request).then( response => {
+            return response || fetch(e.request);
+        })
+    )
+});
